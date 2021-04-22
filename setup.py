aedd387760adf31f01e183a5b2ddc0269772c11f
Gastropod <Bradley.Fallon@gmail.com>
2021-04-22 13:57:58 -0700

    Update setup.py
    Support for all python 3 versions

--- setup.py
+++ setup.py
@@ -20,11 +20,5 @@ setup(
         "Operating System :: OS Independent",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
     ],
 )
